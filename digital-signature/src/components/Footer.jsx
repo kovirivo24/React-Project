e4e9fb1ac8c5b7c544404ac67891f5d15fcd037a
kovirivo24 <60518607+kovitvirivongphotography@users.noreply.github.com>
2023-01-10 14:50:50 -0800

    added hover effect to links, removed button tag to comply with HTML5

--- digital-signature/src/components/Footer.jsx
+++ digital-signature/src/components/Footer.jsx
@@ -6,7 +6,7 @@ import Github from "../images/GitHub-Icon.png"
 import "./Footer.css";
 export default function Footer(){
     return(
-    <nav className='imgs'>
+    <nav className='socials'>
         <a href="https://www.facebook.com/cheng.bryan.9/"><img src={Facebook} alt="Facebook Link"/></a>
         <a href="https://www.instagram.com/_bragor77_/"><img src={Instagram} alt="Instagram Link"/></a>
         <a href="https://github.com/bragor77"><img src={Github} alt="Github Link"/></a>
--- digital-signature/src/components/Info.jsx
+++ digital-signature/src/components/Info.jsx
@@ -7,8 +7,8 @@ export default function Info(){
             <img src={Bryan} alt="Bryan's Portrait" className="info--photo"/>
             <h2 className="info--name"><b>Bryan Cheng</b></h2>
             <h3 className="info--student">3rd Year Aerospace Engineering Student</h3>
-            <a href="mailto:c9cheng@ucsd.edu"><button className="button--email">Email</button></a>
-            <a href="https://www.linkedin.com/in/bryan-cheng-cheuk-hin"><button className="button--LinkedIn">LinkedIn</button></a>
+            <a href="mailto:c9cheng@ucsd.edu"className="button email">Email</a>
+            <a href="https://www.linkedin.com/in/bryan-cheng-cheuk-hin" className="button LinkedIn">LinkedIn</a>
         </nav>
     )
     
