3b5726abf52f58478e7636d2e4ec288cc17e3a08
kovirivo24 <60518607+kovitvirivongphotography@users.noreply.github.com>
2023-01-10 02:39:35 -0800

    fixed buttons and margins

--- digital-signature/src/components/Info.jsx
+++ digital-signature/src/components/Info.jsx
@@ -4,11 +4,11 @@ import "./Info.css";
 export default function Info(){
      return(
         <nav className="info">
-            <img src={Bryan} alt="Bryan's photo" className="info--photo"/>
+            <img src={Bryan} alt="Bryan's Portrait" className="info--photo"/>
             <h2 className="info--name"><b>Bryan Cheng</b></h2>
             <h3 className="info--student">3rd Year Aerospace Engineering Student</h3>
-            <a href="c9cheng@ucsd.edu"><button className="button--email">Email</button></a>
-            <a href="www.linkedin.com/in/bryan-cheng-cheuk-hin"><button className="button--LinkedIn">LinkedIn</button></a>
+            <a href="mailto:c9cheng@ucsd.edu"><button className="button--email">Email</button></a>
+            <a href="https://www.linkedin.com/in/bryan-cheng-cheuk-hin"><button className="button--LinkedIn">LinkedIn</button></a>
         </nav>
     )
     
